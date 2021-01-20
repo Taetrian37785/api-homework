ac47cc1f3dba51d8f39eff49b5b6f07821cce40e
Taetrian37785 <73910099+Taetrian37785@users.noreply.github.com>
2021-01-19 22:00:14 -0500

    Update api_keys.py

--- Homework/starter_code/api_keys.py
+++ Homework/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "fa301e039ca20518362dc876f4133bb4"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBOOBdclWKwnFqbgrZBf2yefezbZ4claF0"
+g_key = ""
